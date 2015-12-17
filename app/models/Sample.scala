f2245e4a01b6c75213c6cfb92827fd05f75bafd3
@satoshi_m8a <satoshi.m8a@gmail.com>
2015-12-17 22:51:27 +0900

    enum map

--- app/models/Sample.scala
+++ app/models/Sample.scala
@@ -4,16 +4,16 @@ import javax.inject.{Inject, Singleton}
 
 import play.api.db.slick.DatabaseConfigProvider
 import slick.driver.JdbcProfile
+import models.Color.Color
 
 import scala.concurrent.ExecutionContext
 
-class Color extends Enumeration {
+object Color extends Enumeration {
   type Color = Value
   val Blue = Value("Blue")
   val Red = Value("Red")
   val Green = Value("Green")
 }
-object Color extends Color
 
 case class Sample(name:String, id:Int, c:Color)
 
@@ -31,9 +31,8 @@ class ColorDao @Inject()(dbConfigProvider: DatabaseConfigProvider)(implicit ec: 
     def * = (name, id, color) <> (Sample.tupled, Sample.unapply)
   }
 
-  def enumStringMapper(enum: Enumeration) = MappedColumnType.base[enum.Value, String](
+  implicit val colorMapper  = MappedColumnType.base[Color, String](
     e => e.toString,
-    s => enum.withName(s)
+    s => Color.withName(s)
   )
-  implicit val colorMapper = enumStringMapper(Color)
 }
